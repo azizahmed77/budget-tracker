26e650e267dd9b35f6785bb3bbd73a1955416baf
aziz <ahmed613@umn.edu>
2022-01-07 21:26:34 -0600

    upload transactions

--- public/js/idb.js
+++ public/js/idb.js
@@ -34,8 +34,54 @@ function saveRecord(record) {
     const transaction = db.transaction(['transactions'], 'readwrite');
   
     // access the object store for `transactions`
-    const transactionsObjectStore = transaction.objectStore('transactions');
+    const budgetTrackerObjectStore = transaction.objectStore('transactions');
   
     // add record to your store with add method
-   transactionsObjectStore.add(record);
+   budgetTrackerObjectStore.add(record);
 }
+
+function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['transactions'], 'readwrite');
+
+    // access your object store
+    const budgetTrackerObjectStore = transaction.objectStore('transactions');
+
+    // get all records from store and set to a variable
+    const getAll = budgetTrackerObjectStore.getAll();
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transactions', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['transactions'], 'readwrite');
+                    // access the new_pizza object store
+                    const budgetTrackerObjectStore = transaction.objectStore('new_pizza');
+                    // clear all items in your store
+                   budgetTrackerObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
