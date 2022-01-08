d9de5c0185fbbb62fc1ae7694f1e7dddc1970ea5
aziz <ahmed613@umn.edu>
2022-01-08 00:19:39 -0600

    manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,14 +1,14 @@
 //creates variable to hold db connection
 let db;
 //establish connection to IndexedDB datababse called 'budget'
-const request = indexedDB.open('budget', 1)
+const request = indexedDB.open('budget_tracker', 1)
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called `transactions, set it to have an auto incrementing primary key of sorts 
-    db.createObjectStore('transactions', { autoIncrement: true });
+    // create an object store (table) called `new_transaction', set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 //upon a successful 
@@ -30,10 +30,10 @@ request.onerror = function(event) {
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['transactions'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
   
-    // access the object store for `transactions`
-    const budgetTrackerObjectStore = transaction.objectStore('transactions');
+    // access the object store for `new_transaction`
+    const budgetTrackerObjectStore = transaction.objectStore('new_transaction');
   
     // add record to your store with add method
    budgetTrackerObjectStore.add(record);
@@ -41,10 +41,10 @@ function saveRecord(record) {
 
 function uploadTransaction() {
     // open a transaction on your db
-    const transaction = db.transaction(['transactions'], 'readwrite');
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
 
     // access your object store
-    const budgetTrackerObjectStore = transaction.objectStore('transactions');
+    const budgetTrackerObjectStore = transaction.objectStore('new_transaction');
 
     // get all records from store and set to a variable
     const getAll = budgetTrackerObjectStore.getAll();
@@ -67,9 +67,9 @@ function uploadTransaction() {
                         throw new Error(serverResponse);
                     }
                     // open one more transaction
-                    const transaction = db.transaction(['transactions'], 'readwrite');
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
                     // access the object store
-                    const budgetTrackerObjectStore = transaction.objectStore('transactions');
+                    const budgetTrackerObjectStore = transaction.objectStore('new_transaction');
                     // clear all items in your store
                    budgetTrackerObjectStore.clear();
 
